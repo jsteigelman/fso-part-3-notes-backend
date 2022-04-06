1dd349b08ce6795e0f18404d81477fd93e072957
Joey Steigelman <joey.steigelman@gmail.com>
2022-04-06 12:55:33 -0400

    set up express, api requests, and VSC REST client for testing api calls

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
 
 let notes = [
     {
@@ -19,12 +22,68 @@ let notes = [
       date: "2022-05-30T19:20:14.298Z",
       important: true
     }
-  ]
-  const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(notes))
+]
+
+app.get('/', (request, response) => {
+response.send('<h1>Hello World!</h1>')
+})
+
+// get all notes
+app.get('/api/notes', (request, response) => {
+response.json(notes)
+})
+
+// get single note
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+})
+
+// generate id for adding single note
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+// add single note
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
+    response.json(note)
+  })
+
+// delete single note
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
   })
 
 const PORT = 3001
-app.listen(PORT)
+app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
+})
+  
