58021378f5618b44da5be7f37d2aad75e4d76bbb
Joey Steigelman <joey.steigelman@gmail.com>
2022-04-07 13:22:23 -0400

    added frontend from fso part 2

---
+++ part2-notes/src/Components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+    const footerStyle = {
+      color: '#f0b3ba',
+      fontStyle: 'italic',
+      fontSize: 16,
+      width: '500px'
+    }
+    return (
+      <div style={footerStyle}>
+        <br />
+        <em>Created by Joey Steigelman | Project provided by Full Stack Open, Department of Computer Science, University of Helsinki 2022</em>
+      </div>
+    )
+  }
+
+export default Footer
