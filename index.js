5768316015b16f998b9f90298401950a5cd47c0c
Joey Steigelman <joey.steigelman@gmail.com>
2022-04-15 17:15:41 -0400

    refactor

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ let notes = [
 ]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+    return response.send('<h1>Hello World!</h1>')
 })
 
 // get all notes
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    return response.json(notes)
 })
 
 // get single note
