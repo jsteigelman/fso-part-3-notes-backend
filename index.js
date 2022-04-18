ca4cf8ce058c9ec799760904ee6cf70dca4214f0
Joey Steigelman <joey.steigelman@gmail.com>
2022-04-18 10:35:10 -0400

    use database in route handlers

--- index.js
+++ index.js
@@ -58,13 +58,9 @@ app.get('/api/notes', (request, response) => {
 
 // get single note
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
 
 // generate id for adding single note
@@ -76,26 +72,23 @@ const generateId = () => {
   }
 
 // add single note
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
-  
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const note = {
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
   })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
 
 // delete single note
 app.delete('/api/notes/:id', (request, response) => {
